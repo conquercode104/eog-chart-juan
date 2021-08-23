413beb53268521a4b0ee8ee696ed0dd134dd6a99
JuanSalazar <zairesagical@gmail.com>
2021-08-22 20:33:29 -0700

    updated Wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -4,6 +4,10 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   wrapper: {
     height: '100vh',
+    overflow: 'auto',
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr',
+    boxSizing: 'border-box',
   },
 });
 
